068a67560853e10f2f76ffbe8fcaf2ba7eab8299
AnkitTripathi <ankit.scrps@gmail.com>
2025-01-13 04:31:38 +0530

    controls schedular sending mail by flag initilally set to false

--- src/main/java/com/otsMail/service/EmailSchedulerService.java
+++ src/main/java/com/otsMail/service/EmailSchedulerService.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -21,21 +22,27 @@ public class EmailSchedulerService {
 	private final @NonNull MailService mailService;
 	private final @NonNull EmailHelper emailHelper;
 
+	@Value("${scheduled.enabled:false}")  // Default to false if not set
+    private boolean isScheduledEnabled;
 	/**
 	 * Sends Email to recipient with status as active and  subscription as true every Monday at 10AM.
 	 */
 	@Scheduled(cron = "0 0 10 * * MON")
 	public void sendEmailsEveryMondayAt10AM() {
-		log.info("Sending emails every Monday at 10 Am to all recipient with status as active and subscription as true: {} ", LocalDateTime.now());
-		List<Recipient> recipients = emailHelper.getActiveEnrollments()
-								.stream()
-								.map(enroll -> Recipient.builder()
-											.email(enroll.getTo())
-											.salutation(enroll.getSalutation())
-											.build())
-								.collect(Collectors.toList());
-		for (Recipient recipient : recipients) {
-			mailService.sendEmailToRecipient(recipient);
+	       if(isScheduledEnabled) {
+			log.info("Sending emails every Monday at 10 Am to all recipient with status as active and subscription as true: {} ", LocalDateTime.now());
+			List<Recipient> recipients = emailHelper.getActiveEnrollments()
+									.stream()
+									.map(enroll -> Recipient.builder()
+												.email(enroll.getTo())
+												.salutation(enroll.getSalutation())
+												.build())
+									.collect(Collectors.toList());
+			for (Recipient recipient : recipients) {
+				mailService.sendEmailToRecipient(recipient);
+			}
+		}else {
+			log.info("Schedular set to false");
 		}
 	}
 
